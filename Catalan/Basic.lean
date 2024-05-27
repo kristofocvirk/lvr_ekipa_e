7f83033bc50d54e9eff0bcb0b4bcec2f7f8512f6
kristof ocvirk <kristof.ocvirk@gmail.com>
2024-05-27 21:08:30 +0200

    add first proof

--- Catalan/Basic.lean
+++ Catalan/Basic.lean
@@ -33,31 +33,6 @@ inductive full_binary_tree_with_nodes : ℕ → Type
 | join : {n m : ℕ} → full_binary_tree_with_nodes n → full_binary_tree_with_nodes m →
           full_binary_tree_with_nodes (n + m + 1)
 
-def full_binary_tree.height : full_binary_tree → ℕ
-| .leaf => 0
-| .join T1 T2 => 1 + max T1.height T2.height
-
-def full_binary_tree.number_of_leaves :
-  full_binary_tree → ℕ
-| .leaf => 1
-| .join T1 T2 => T1.number_of_leaves + T2.number_of_leaves
-
-def plane_tree_nodes : plane_tree → ℕ
-| .node [] => 0
-| .node (tr :: []) => plane_tree_nodes tr + 1
-| .node (tr :: trs) => plane_tree_nodes tr + plane_tree_nodes (plane_tree.node trs) + 1
-
-def plane_tree_of_full_binary_tree :
-  full_binary_tree → plane_tree
-  | .leaf => plane_tree.node []
-  | .join T1 T2 => plane_tree.node (plane_tree_of_full_binary_tree T1 :: plane_tree_of_full_binary_tree T2 :: [])
-
-def full_binary_tree_of_plane_tree :
-  plane_tree → full_binary_tree
-  | .node [] => .leaf
-  | .node (tr :: []) => full_binary_tree_of_plane_tree tr
-  | .node (tr :: trs) => .join (full_binary_tree_of_plane_tree tr) (full_binary_tree_of_plane_tree (plane_tree.node trs))
-
 /-small task 5-/
 
 inductive ballot_seq : ℕ → Type
@@ -78,24 +53,79 @@ def is_valid_seq : ballot_seq n → Prop
 /-big task 1-/
 def fin_pi_fin_equiv  {m : ℕ} {n : Fin m → ℕ} : ((i : Fin m) → Fin (n i)) ≃ Fin (Finset.univ.prod fun (i : Fin m) => n i) :=
 sorry
-/-big task 2-/
 
 /-big task 3-/
 def bijectionFullBinTreeCatalan (n : ℕ) : (full_binary_tree_with_nodes n) ≃ Fin (catalan (n+1)) :=
 sorry
 
 /-big task 4-/
-def list_plane_tree_equiv_plane_tree : List plane_tree ≃ plane_tree :=
-sorry
+def list_plane_tree_to_plane_tree :
+  List plane_tree → plane_tree
+  | [] => .node []
+  | tr :: trs => .node (tr :: trs)
+
+def plane_tree_to_list_plane_trees :
+  plane_tree → List plane_tree
+  | .node [] => []
+  | .node (tr :: trs)  => tr :: trs
+
+def list_plane_tree_equiv_plane_tree : List plane_tree ≃ plane_tree where
+toFun := list_plane_tree_to_plane_tree
+invFun := plane_tree_to_list_plane_trees
+left_inv := by
+  intro ls
+  cases' ls with hd tl
+  . simp [list_plane_tree_to_plane_tree]
+    simp [plane_tree_to_list_plane_trees]
+  . simp [plane_tree_to_list_plane_trees]
+    simp [list_plane_tree_to_plane_tree]
+right_inv := by
+  intro T
+  cases' T with trs
+  . induction' trs
+    . simp [plane_tree_to_list_plane_trees, list_plane_tree_to_plane_tree]
+    . simp [plane_tree_to_list_plane_trees, list_plane_tree_to_plane_tree]
 
 /-big task 5-/
-def plane_tree_equiv_full_binary_tree : plane_tree ≃ full_binary_tree :=
-sorry
+def plane_tree_of_full_binary_tree :
+  full_binary_tree → plane_tree
+  | .leaf => plane_tree.node []
+  | .join T1 T2 => match plane_tree_of_full_binary_tree T2 with | .node trs => .node ((plane_tree_of_full_binary_tree T1) :: trs)
+
+def full_binary_tree_of_plane_tree :
+  plane_tree → full_binary_tree
+  | .node [] => .leaf
+  | .node (tr :: []) => full_binary_tree_of_plane_tree tr
+  | .node (tr :: trs) => .join (full_binary_tree_of_plane_tree tr) (full_binary_tree_of_plane_tree (plane_tree.node trs))
+
+#check Function.LeftInverse
+#check Function.RightInverse
+#check Nat.zero_le
+
+
+def plane_tree_equiv_full_binary_tree : plane_tree ≃ full_binary_tree where
+toFun := full_binary_tree_of_plane_tree
+invFun := plane_tree_of_full_binary_tree
+left_inv := by
+  intro T
+  cases' T with trs
+  induction' trs with hd tl Htl
+  . simp [full_binary_tree_of_plane_tree, plane_tree_of_full_binary_tree]
+  . sorry
+right_inv := by
+  intro T
+  induction' T with _ _ TH1 TH2
+  . simp [plane_tree_of_full_binary_tree, full_binary_tree_of_plane_tree]
+  . simp [plane_tree_of_full_binary_tree]
+    sorry
+
+
+
 
 /-big task 6 -/
 
 theorem divisor_of_choose_2n_plus_one (n : ℕ) : (Nat.choose (2*n) (n+1)) = n / (n+1) * (Nat.choose (2*n) n) := by
-sorry
+  sorry
 
 /-big task 7-/
 theorem catalan_n_eq_2n_choose_n (n : ℕ) : catalan n = (Nat.choose (2*n) n) / (n+1) := by
